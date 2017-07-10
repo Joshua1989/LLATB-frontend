a6cd7b4490b6c32aa726c7cae55655098128bf74
Mengke Lian <ml311@duke.edu>
2017-07-09 21:54:41 -0400

    use postgres database

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
